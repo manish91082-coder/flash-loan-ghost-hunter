42770c2f5c45dfca8d86a408d255e5079409dcea
manish91082-coder <manish91082@gmail.com>
2026-09-10 00:45:56 +0530

    tests: correct optimizer fixture and expected optimum

--- tests/test_loan_optimizer.py
+++ tests/test_loan_optimizer.py
@@ -40,7 +40,7 @@ class LoanOptimizerTests(unittest.TestCase):
 
     def test_selects_best_real_quote_not_max_allowed_loan(self):
         def sampler(loan):
-            # Executable output peaks near $20k due to deteriorating price impact.
+            # The implemented surface has its mathematical maximum near $40k.
             gain = D("0.004") * loan - D("0.0000001") * (loan - D("20000")) ** 2
             out = loan + gain
             return [
@@ -57,8 +57,8 @@ class LoanOptimizerTests(unittest.TestCase):
             ),
         )
         self.assertIsNotNone(cert)
-        self.assertGreater(cert.loan_usd, D("10000"))
-        self.assertLess(cert.loan_usd, D("30000"))
+        self.assertGreater(cert.loan_usd, D("30000"))
+        self.assertLess(cert.loan_usd, D("50000"))
 
     def test_sampler_failure_is_blocked(self):
         def sampler(_):
